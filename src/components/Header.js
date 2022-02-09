748fcaa63e1e71daa5cbcf268a32e3b7a4b81ee3
Douglas Maringa <douglasmaringa4@gmail.com>
2022-02-09 15:17:43 +0200

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -62,7 +62,7 @@ function Header() {
                            {/* <!-- search -->*/}
                             <a href="search.html" class="widget-header mr-4 text-dark">
                                 <div class="icon d-flex align-items-center">
-                                    <i class="feather-search h6 mr-2 mb-0"></i> <span>Search</span>
+                                    
                                 </div>
                             </a>
                              {/* <!-- offers -->*/}
@@ -103,7 +103,6 @@ function Header() {
                                    <div class="dropdown-menu dropdown-menu-right" aria-labelledby="dropdownMenuButton">
                                     <button class="dropdown-item" id="link" onClick={()=>{navigate("/adminorders")}}>View Orders</button>
                                     <button class="dropdown-item" id="link" onClick={()=>{navigate("/admintype")}}>Create Meal</button>
-                                    <button class="dropdown-item" id="link" onClick={()=>{navigate("/adminmenu")}}>Create Menu</button>
                                 </div>
                                    </>):(<>
                                    {
@@ -142,20 +141,20 @@ function Header() {
         {/* <!-- header main -->*/}
     </header>
     <div class="osahan-home-page">
-        <div class="bg-primary p-3 d-none">
+        <div class="bg-success p-3 d-none">
             <div class="text-white">
                 <div class="title d-flex align-items-center">
                     
-                    <h4 class="font-weight-bold m-0 pl-5">Browse</h4>
-                    <a class="text-white font-weight-bold ml-auto" data-toggle="modal" data-target="#exampleModal">Filter</a>
+                    <h4 class="font-weight-bold m-0 pl-5">Sinurame</h4>
+                    <div class="col-1">
+                        
+                            <img alt="#" class="img-fluid" id="link" onClick={()=>{navigate("/")}} src="https://i.ibb.co/YLhZNL8/IMG-20220124-WA0037.jpg" width="70" height="20"/>
+                        
+                      
+                    </div>
                 </div>
             </div>
-            <div class="input-group mt-3 rounded shadow-sm overflow-hidden">
-                <div class="input-group-prepend">
-                    <button class="border-0 btn btn-outline-secondary text-dark bg-white btn-block"><i class="feather-search"></i></button>
-                </div>
-                <input type="text" class="shadow-none border-0 form-control" placeholder="Search for restaurants or dishes"/>
-            </div>
+
         </div>
        
        
